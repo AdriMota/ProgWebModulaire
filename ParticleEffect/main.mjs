92860bd6aeb456879aa1460352cb1f42a9724cbd
Adriana Mota <adrianamota17@hotmail.com>
2022-01-12 15:07:19 +0100

    Sprite for particle effect

--- ParticleEffect/main.mjs
+++ ParticleEffect/main.mjs
@@ -1,7 +1,8 @@
 import Sprite from "../class/ParticleEffect/Sprite.js";
+import Tweens from "../class/Tweens.js";
 import MainLoop from "../lib/mainloop.js";
-//import * as random from "../lib/randomInt.mjs";
-//import randomColor from "../lib/randomColor.mjs";
+import * as random from "../lib/randomInt.mjs";
+import { domOn } from "../lib/dom.js";
 
 const ctx = document.querySelector('canvas').getContext('2d');
 
@@ -9,19 +10,93 @@ const ctx = document.querySelector('canvas').getContext('2d');
 ctx.canvas.height = ctx.canvas.clientHeight;
 ctx.canvas.width = ctx.canvas.clientWidth;
 
-const sprite = new Sprite();
+const sprites = new Array(300);
+const imgHeight = 64;
+const imgWidth = 64;
+let mouse = { x: -100, y: -100, height: 200, width: 200 };
 
+for (let i = 0; i < sprites.length; i++) {
 
+    const randDenominateur = random.getInt(2, 5);
+
+    sprites[i] = new Sprite({
+        x: random.getInt(0, ctx.canvas.width),
+        y: random.getInt(0, ctx.canvas.height),
+        width: Math.round(imgWidth / randDenominateur),
+        height: Math.round(imgHeight / randDenominateur),
+        color: `hsl(0, 100%, 50%)`,
+        velX: (Math.random() - 0.5) / 5,
+        velY: (Math.random() - 0.5) / 5
+    })
+
+}
+
+const tweens = new Tweens();
+
+tweens.create({
+    to: 360,
+    duration: 50000,
+    loop: true,
+    animate: hue => {
+        sprites.forEach(sprite => {
+            sprite.setColor(`hsl(${hue}, 100%, 50%)`);
+        })
+    }
+})
+
+domOn('canvas', 'mousemove', event => {
+    const rect = ctx.canvas.getBoundingClientRect();
+
+    mouse.x = event.clientX - rect.left;
+    mouse.y = event.clientY - rect.top;
+
+    sprites.forEach(sprite => {
+        let collision = sprite.x < mouse.x + mouse.width &&
+            sprite.x + sprite.width > mouse.x &&
+            sprite.y < mouse.y + mouse.height &&
+            sprite.height + sprite.y > mouse.y
+            ? true
+            : false;
+
+        if (collision) {
+            const velX = Math.atan2(sprite.x - mouse.x, sprite.y - mouse.y) / 6;
+
+            sprite.setVelX(velX);
+        }
+    })
+})
 
 MainLoop.setUpdate(dt => {
-    
+    tweens.update(dt, ctx.canvas.width, ctx.canvas.height);
+
+    for (const sprite of sprites) {
+        sprite.move(dt);
+        sprite.bounceOffTheWalls(ctx);
+    }
+
 });
 
 MainLoop.setDraw(() => {
     ctx.canvas.height = ctx.canvas.clientHeight;
     ctx.canvas.width = ctx.canvas.clientWidth;
 
-    sprite.draw(ctx, ctx.canvas.width, ctx.canvas.height);
+    sprites.forEach(sprite => sprite.draw(ctx));
+
+    // set composite mode
+    ctx.globalCompositeOperation = "source-in";
+
+    // draw color
+    ctx.fillStyle = sprites[0].color;
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    // reset comp. mode
+    ctx.globalCompositeOperation = "source-over";
+
 });
+
+MainLoop.setEnd((fps, panic) => {
+    if (panic) console.log('panic');
+
+})
 
 MainLoop.start();
--- class/ParticleEffect/Sprite.js
+++ class/ParticleEffect/Sprite.js
@@ -7,7 +7,7 @@ export default class Sprite {
     // de façon à ce qu'on puisse saisir les données (x, y, r ...) dans
     // le désordre. Le = {} dans la parenthèse permet de créer un cercle
     // sans indiquer les valeurs des différents paramètres
-    constructor({src = "../../Images/helmet.png", x = 0, y = 0, width = 50, height = 50} = {}) { 
+    constructor({src = "../../Images/helmet.png", x = 0, y = 0, width = 50, height = 50, color = 'pink', velX = 0, velY = 0 } = {}) { 
         this.src = src;
         this.image = new Image;
         this.image.src = this.src;
@@ -15,24 +15,54 @@ export default class Sprite {
         this.y = y;
         this.width = width;
         this.height = height;
+        this.color = color;
+        this.velX = velX;
+        this.velY = velY;
 
         //this._x = x; --> Le _ empêche d'accéder à la propriété en dehors de l'objet
         // this.#r = r; --> Le # protège la donnée pour ne pas qu'elle change de valeur
     }
 
-    draw(ctx, canvasWidth, canvasHeight) {
+    setColor(color) {
+        this.color = color;
+    }
+
+    setVelX(velX) {
+        this.velX = velX;
+    }
+
+    draw(ctx) {
         // draw image
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-      
-        // set composite mode
-        ctx.globalCompositeOperation = "source-in";
-      
-        // draw color
-        ctx.fillStyle = "#09f";
-        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-
-        // reset comp. mode
-        ctx.globalCompositeOperation = "source-over";
     }
-       
+
+    move(dt) {
+        this.x += this.velX * dt;
+        this.y += this.velY * dt;
+    }
+
+    bounceOffTheWalls(ctx) {
+        let width = ctx.canvas.width;
+        let height = ctx.canvas.height;
+
+        if (this.x + this.width > width) {
+          this.velX = -Math.abs(this.velX);
+          this.x = width - this.width;
+        }
+
+        if (this.x < 0) {
+          this.velX = +Math.abs(this.velX);
+          this.x = 0;
+        }
+
+        if (this.y + this.height > height) {
+          this.velY = -Math.abs(this.velY);
+          this.y = height - this.height;
+        }
+
+        if (this.y < 0) {
+          this.velY = +Math.abs(this.velY);
+          this.y = 0;
+        }  
+    }
 }
